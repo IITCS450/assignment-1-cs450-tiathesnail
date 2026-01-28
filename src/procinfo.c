b8237b2eda4c48bdcc578f59084e4729e9de1a47
tia johnson <ljohnson23hawk.illinoistech.edu>
2026-01-27 21:51:02 -0600

    save

--- src/procinfo.c
+++ src/procinfo.c
@@ -6,6 +6,55 @@ static void usage(const char *a){fprintf(stderr,"Usage: %s <pid>\n",a); exit(1);
 static int isnum(const char*s){for(;*s;s++) if(!isdigit(*s)) return 0; return 1;}
 int main(int c,char**v){
  if(c!=2||!isnum(v[1])) usage(v[0]);
- printf("TODO: implement procinfo\n");
+ char filepath[100];
+ sprintf(filepath,"/proc/%s/stat",v[1]);
+ char statusFilepath[100];
+ sprintf(statusFilepath,"/proc/%s/status",v[1]);
+ char cmdlineFilepath[100];
+ sprintf(cmdlineFilepath,"/proc/%s/cmdline",v[1]);
+
+ char infoString[100];
+ FILE *statusFile = fopen(statusFilepath,"r");
+ if(statusFile == NULL){
+ 	printf("pid does not correspond to an existing process.\n");
+	return 0;
+ }else{
+	 while(fgets(infoString,100,statusFile)) {
+		 long p = 0;
+		 p |= (long) strstr(infoString,"State");
+		 p |= (long) strstr(infoString,"PPid");
+		 p |= (long) strstr(infoString,"VmRSS");
+		 if(p){
+			 printf("%s",infoString);
+		 }
+
+	 } 
+	 fclose(statusFile);
+ }
+ FILE *cmdlineFile = fopen(cmdlineFilepath,"r");
+ if(cmdlineFile != NULL){
+ 	if(fgets(infoString,100,cmdlineFile)){
+	printf("Cmd: %s\n", infoString);
+	}
+	fclose(cmdlineFile);
+ }
+ FILE *statFile = fopen(filepath, "r");
+ if(statFile){
+	 int cpu_time = 0;
+	 long bleh =0;
+	 fscanf(statFile, "%[^)]) %s ", infoString, infoString);
+	 for(int i=4; i<16;i++){
+	 	fscanf(statFile, "%s ", infoString);
+		if( i==14 || i == 15){
+			sscanf(infoString,"%ld", &bleh);
+			cpu_time+=bleh;
+		}
+	 }
+	 printf("%ld\n", cpu_time);
+	 double timeSeconds = (double)cpu_time / sysconf(_SC_CLK_TCK);
+	 printf("CPU time: %.9f\n", timeSeconds);
+	 
+	 fclose(statFile);
+ }
  return 0;
 }
